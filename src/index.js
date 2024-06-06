c3ddc361c98d434dd28cbbd1ac2177571a1d118a
polinamamchur <polinamam04@gmail.com>
2024-06-06 09:13:00 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import react from "react";
-import reactDom from "react-dom";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
+const App = () => (
+  <div>
+    Hello, React!
+  </div>
+);
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(<App />);
