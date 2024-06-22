6616160d799251e1c04c969768f58cc12744017a
polinamamchur <polinamam04@gmail.com>
2024-06-22 22:51:03 +0300

    Test pre-commit hook

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
 const App = () => {
   return (
     <div>
       <p>Hello from React !!!!</p>
     </div>
-  )
-}
+  );
+};
 
-const root = createRoot(document.getElementById('app'))
-root.render(<App />)
+const root = createRoot(document.getElementById('app'));
+root.render(<App />);
