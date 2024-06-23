6f8c56d25a3a708acb4a6b59c057d9a04d33580c
polinamamchur <polinamam04@gmail.com>
2024-06-23 15:26:06 +0300

    Test husky and lint-staged

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
 const App = () => {
   return (
     <div>
       <p>Hello from React !!!!</p>
     </div>
-  );
-};
+  )
+}
 
-const root = createRoot(document.getElementById('app'));
-root.render(<App />);
+const root = createRoot(document.getElementById('app'))
+root.render(<App />)
 
-// test pre-hook commit
+// test pre-hook commit2
