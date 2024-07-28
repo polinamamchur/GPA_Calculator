a43112e1228e7844bba259cef44a2834a87189da
polinamamchur <polinamam04@gmail.com>
2024-07-28 13:36:34 +0300

    app-impr

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import userClient from './clients/user'
 
-userClient.init('https://logindb-a4df.restdb.io', process.env.API_KEY, fetch)
+userClient.init('https://logindb-a4df.restdb.io', '7d3ebc7e75ad59998d8cda1cbf02efc8a68e5', fetch)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
